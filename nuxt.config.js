ebb4e2798fda93f5fbc1346c63576a6b0d6ca98d
Evgeny Vlasov <evgen@mobsted.com>
2020-09-04 16:47:53 +0500

    start project

---
+++ nuxt.config.js
@@ -0,0 +1,101 @@
+import data from "./static/storedata.json";
+let dynamicRoutes = () => {
+  return new Promise((resolve) => {
+    resolve(data.map((el) => `product/${el.id}`));
+  });
+};
+
+export default {
+  mode: "universal",
+  /*
+   ** Headers of the page
+   */
+  head: {
+    title: "Skyline Ivy",
+    script: [
+      { src: "https://js.stripe.com/v3/" },
+      {
+        src: "https://cdn.jsdelivr.net/npm/pwacompat",
+        crossorigin: "anonymous",
+        assets: true
+      },
+      {
+        innerHTML: `
+          (function(m,o,b,s,t,e,d){
+            m[t]=m[t]||{},e=o.createElement(b),d=o.getElementsByTagName(b)[0],
+              e.async=1,e.src=s,d.parentNode.insertBefore(e,d)
+          })(window,document,"script","https://widgets.mobsted.com/w4pwa.js","w4pwa");
+          w4pwa.config = {
+            accountId: "a7db574a",
+            projectId: 93,
+            clientId: 93,
+          }
+        `
+      }
+    ],
+    meta: [
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+
+      {
+        hid: "description",
+        name: "description",
+        content: process.env.npm_package_description || ""
+      }
+    ],
+    link: [
+      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+      {
+        rel: "stylesheet",
+        href:
+          "https://fonts.googleapis.com/css?family=Montserrat:300,600|PT+Serif&display=swap"
+      }
+    ],
+    __dangerouslyDisableSanitizers: ["script"]
+  },
+  generate: {
+    routes: dynamicRoutes
+  },
+  /*
+   ** Customize the progress-bar color
+   */
+  loading: { color: "#fff" },
+  /*
+   ** Global CSS
+   */
+  css: ["normalize.css", { src: "~/assets/main.scss", lang: "sass" }],
+  /*
+   ** Plugins to load before mounting the App
+   */
+  plugins: [`~/plugins/currency-filter.js`],
+  /*
+   ** Nuxt.js modules
+   */
+  modules: ["@nuxtjs/pwa"],
+  pwa: {
+    icon: {
+      fileName: "icon512x512.png"
+    },
+    manifest: {
+      name: "Skyline Ivy",
+      short_name: "SI store",
+      start_url: "/?source=pwa",
+      description: "My firts PWA website",
+      background_color: "#2b2b2b",
+      theme_color: "#2b2b2b",
+      display: "standalone"
+    },
+    workbox: {
+      enabled: true
+    }
+  },
+  /*
+   ** Build configuration
+   */
+  build: {
+    /*
+     ** You can extend webpack config here
+     */
+    extend(config, ctx) {}
+  }
+};
---
+++ pages/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <app-textlockup />
+    <app-sales-boxes />
+    <app-featured-products />
+  </div>
+</template>
+
+<script>
+import AppTextlockup from "~/components/AppTextlockup.vue";
+import AppSalesBoxes from "~/components/AppSalesBoxes.vue";
+import AppFeaturedProducts from "~/components/AppFeaturedProducts.vue";
+
+export default {
+  components: {
+    AppTextlockup,
+    AppSalesBoxes,
+    AppFeaturedProducts
+  }
+};
+</script>
+
+<style>
+.test {
+  margin: 3vh;
+}
+
+main {
+  width: 75vw;
+}
+</style>
